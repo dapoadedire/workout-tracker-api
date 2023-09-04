14417381a77b073422210d08636803643b6c7ab8
dapoadedire <adedireadedapo19@gmail.com>
2023-09-04 10:30:22 +0100

    🚀feat(config): add MongoDB URI validation 🛠️refactor(config): improve MongoDB connection message

--- config/workout.db.js
+++ config/workout.db.js
@@ -4,6 +4,15 @@ dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
+
+
+
 export const connecToMongoDB = () => {
   console.log("Trying to connect to MongoDB...");
   
